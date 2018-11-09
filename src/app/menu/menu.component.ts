6f32748c59db8623b5551b16cc81185524fd1394
Montgomery Nugen <montgomery.nugen@gmail.com>
2018-11-09 11:03:37 -0500

    changes

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  menus = [
+    { display: 'Home', href: 'http://www.maxtrain.com', title: 'MAX'},
+    { display: 'Google', href: 'http://www.google.com', title: 'Googled!'},
+    { display: 'Microsoft', href: 'http://www.microsoft.com', title: 'Evil Incarnate!'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
